36d8b9ed0d305c51c6d3cc9e1c4adf05f8847398
Jordyn Fulbright <jordynfulbright@gmail.com>
2023-04-11 18:35:40 -0400

    Initial commit

---
+++ app/src/main/java/edu/uga/cs/rideshare/MainActivity.java
@@ -0,0 +1,71 @@
+package edu.uga.cs.rideshare;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "RideShare";
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+        String email = "dawg@mail.com";
+        String password = "password";
+
+
+
+        mAuth.signInWithEmailAndPassword( email, password )
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+// Sign in success
+                            Log.d( TAG, "signInWithEmail:success" );
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        }
+                        else {
+// If sign in fails
+                            Log.d( TAG, "signInWithEmail:failure", task.getException() );
+                        }
+                    }
+                });
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        //this key doesnt exist in the db rn
+        DatabaseReference myRef = database.getReference( "message" );
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+// This method is called once, initially, and when data is updated
+                String message = dataSnapshot.getValue(String.class);
+    //could set text here
+            }
+            @Override
+            public void onCancelled( DatabaseError error ) {
+// Failed to read value
+                Log.d( TAG, "Failed to read value.", error.toException() );
+            }
+        });
+
+
+    }
+}
