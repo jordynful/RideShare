a9d669b2d46825a7edd1b39225f0a590ed61c752
Jordyn Fulbright <jordynfulbright@gmail.com>
2023-04-17 00:42:18 -0400

    added start activity and login functionality

--- app/src/main/java/edu/uga/cs/rideshare/LogInActivity.java
+++ app/src/main/java/edu/uga/cs/rideshare/LogInActivity.java
@@ -1,13 +1,26 @@
 package edu.uga.cs.rideshare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogInActivity extends AppCompatActivity {
     private EditText mEmailEditText;
+    public static final String TAG = "LOGIN ACTIVITY";
     private EditText mPasswordEditText;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +29,38 @@ public class LogInActivity extends AppCompatActivity {
         mEmailEditText = findViewById(R.id.editTextTextEmailAddress);
         mPasswordEditText = findViewById(R.id.editTextTextPassword);
 
+        Button loginButton = findViewById(R.id.button3);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth = FirebaseAuth.getInstance();
+                // Get the email and password from the EditText views
+                String email = mEmailEditText.getText().toString();
+                String password = mPasswordEditText.getText().toString();
 
+
+                mAuth.signInWithEmailAndPassword( email, password )
+                        .addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+// Sign in success
+                                    Log.d( TAG, "signInWithEmail:success" );
+                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    Intent intent = new Intent(LogInActivity.this, HomeActivity.class);
+                                    startActivity(intent);
+                                }
+                                else {
+// If sign in fails
+                                    Log.d( TAG, "signInWithEmail:failure", task.getException() );
+                                }
+                            }
+                        });
+
+                // Send the email and password to your database to check if they are valid
+                // Here you would call a method or function that handles the database logic
+                // and checks if the email and password are valid.
+            }
+        });
     }
 }
---
+++ app/src/main/java/edu/uga/cs/rideshare/StartActivity.java
@@ -0,0 +1,36 @@
+package edu.uga.cs.rideshare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button loginButton = findViewById(R.id.button2);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, LogInActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button signupButton = findViewById(R.id.button4);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
