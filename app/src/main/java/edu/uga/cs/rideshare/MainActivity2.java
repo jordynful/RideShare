94be1eae9ab414dc01dbacdf98fc8821af53eda1
Jordyn Fulbright <jordynfulbright@gmail.com>
2023-04-30 16:22:56 -0400

    nav drawer

--- app/src/main/java/edu/uga/cs/rideshare/MainActivity2.java
+++ app/src/main/java/edu/uga/cs/rideshare/MainActivity2.java
@@ -1,16 +1,29 @@
 package edu.uga.cs.rideshare;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity2 extends AppCompatActivity {
-private Button button;
+    private Button button;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private ActionBarDrawerToggle drawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,15 +43,112 @@ private Button button;
             }
         });
 
-//        Home homeFragment = new Home();
+        Home homeFragment = new Home();
 //        RequestRide requestRideFragment = new RequestRide();
 //        OfferRide offerRideFragment = new OfferRide();
 //        CurrentRequests currentRequestsFragment = new CurrentRequests();
 //        CurrentOffers currentOffersFragment = new CurrentOffers();
-        Rides ridesFragment = new Rides();
+//        Rides ridesFragment = new Rides();
+
+
+
+
+        toolbar = findViewById( R.id.toolbar );
+
+        // using toolbar as ActionBar
+        setSupportActionBar( toolbar );
+
+        // Find our drawer view
+        drawerLayout = (DrawerLayout) findViewById( R.id.drawer_layout );
+        drawerToggle = setupDrawerToggle();
+
+        drawerToggle.setDrawerIndicatorEnabled( true );
+        drawerToggle.syncState();
+
+        // Connect DrawerLayout events to the ActionBarToggle
+        drawerLayout.addDrawerListener( drawerToggle );
+
+        // Find the drawer view
+        navigationView = findViewById( R.id.nvView );
+        navigationView.setNavigationItemSelectedListener(
+                menuItem -> {
+                    navigateBasedOnDrawerItem( menuItem );
+                    return true;
+                });
 
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.frameLayout, ridesFragment)
+                .replace(R.id.frameLayout, homeFragment)
                 .commit();
+    }
+
+    public void navigateBasedOnDrawerItem(MenuItem menuItem ) {
+        Fragment fragment = null;
+
+        // Create a new fragment based on the used selection in the nav drawer
+        switch( menuItem.getItemId() ) {
+            case R.id.menu_requests:
+                fragment = new CurrentRequests();
+                break;
+            case R.id.menu_current_offers:
+                fragment = new CurrentOffers();
+                break;
+            case R.id.menu_help:
+                fragment = new HelpFragment();
+                break;
+            case R.id.menu_home:
+                fragment = new Home();
+                break;
+            case R.id.menu_close:
+                finish();
+                break;
+            default:
+                return;
+        }
+
+        // Set up the fragment by replacing any existing fragment in the main activity
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment currentFragment = fragmentManager.findFragmentById(R.id.frameLayout);
+        int currentFragmentId = currentFragment.getId();
+        fragmentManager.beginTransaction().replace(currentFragmentId, fragment ).addToBackStack( "home" ).commit();
+
+        /*
+        // this is included here as a possible future modification
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked( true );
+        // Set action bar title
+        setTitle( menuItem.getTitle());
+         */
+
+        // Close the navigation drawer
+        drawerLayout.closeDrawers();
+    }
+
+    private ActionBarDrawerToggle setupDrawerToggle() {
+        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+        // and will not render the hamburger icon without it.
+        return new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open,  R.string.drawer_close );
+    }
+
+    // onPostCreate is called when activity start-up is complete after onStart()
+    @Override
+    protected void onPostCreate( Bundle savedInstanceState ) {
+        super.onPostCreate( savedInstanceState );
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged( @NonNull Configuration newConfig ) {
+        super.onConfigurationChanged( newConfig );
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged( newConfig );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item ) {
+        if( drawerToggle.onOptionsItemSelected( item ) ) {
+            return true;
+        }
+        return super.onOptionsItemSelected( item );
     }
 }
